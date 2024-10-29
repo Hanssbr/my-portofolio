ec26de2d5b21a6d521474d20245bb96816b956be
Hanssbr <rayhaniqbalbr@gmail.com>
2024-10-29 18:20:55 +0700

    baru

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**\*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-black': '#1A1A19',
+        'custom-dark-green': '#31511E'
+      },
+      top: {
+        '120': '28rem',
+      },
+    },
+  },
+  plugins: [],
+}
+
