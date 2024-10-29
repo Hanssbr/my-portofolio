20dfd2d0277dd49167bd91ee9897e6699fc4c5da
Hanssbr <rayhaniqbalbr@gmail.com>
2024-10-29 20:48:01 +0700

    ''

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**\*.{html,js}"],
+  content: ["./***.{html,js}"],
   theme: {
     extend: {
       colors: {
-        'custom-black': '#1A1A19',
-        'custom-dark-green': '#31511E'
+        "custom-black": "#1A1A19",
+        "custom-dark-green": "#31511E",
+        "custom-pink": "#FFCCEA",
       },
       top: {
-        '120': '28rem',
-        '140': '34rem',
+        120: "28rem",
+        121: "34rem",
       },
       right: {
-        '120': '20rem'
+        122: "27rem",
       },
       fontFamily: {
-        'poppins': ['Poppins', 'sans-serif']
+        poppins: ["Poppins", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
-
+};
